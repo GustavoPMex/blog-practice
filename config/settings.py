18116488bf9a467ea8d1f2127911726c9df7b851
Gustavo Mex <gustavoppymex@gmail.com>
2022-03-26 20:56:27 -0600

    Modified heroku

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise',
+    'whitenoise.runserver_nostatic',
     'blog',
     'accounts',
 ]
